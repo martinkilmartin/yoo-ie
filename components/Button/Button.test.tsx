8d7d48b880d83a52d7ef23b5828b9456c491ff03
sionnach <sionnach.solutions@gmail.com>
2021-07-31 22:03:43 -0400

    Adds daisyui button utilities

--- components/Button/Button.test.tsx
+++ components/Button/Button.test.tsx
@@ -6,3 +6,17 @@ test('Button displays text', () => {
   const tree = button.toJSON()
   expect(tree).toMatchSnapshot()
 })
+
+test('Button displays text and is small', () => {
+  const button = renderer.create(<Button text="Push Me" size="sm" />)
+  const tree = button.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Button displays text and is small and wide', () => {
+  const button = renderer.create(
+    <Button text="Push Me" size="sm" shape="wide" />
+  )
+  const tree = button.toJSON()
+  expect(tree).toMatchSnapshot()
+})
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,9 +1,100 @@
-type Props = {
-  text: string
+import clsx from 'clsx'
+
+const SIZES = {
+  no: '',
+  lg: ' btn-lg',
+  md: ' btn-md',
+  sm: ' btn-sm',
+  xs: ' btn-xs',
 }
 
-const Button = ({ text }: Props): JSX.Element => (
-  <button className="btn">{text}</button>
-)
+const SHAPES = {
+  no: '',
+  wide: ' btn-wide',
+  block: ' btn-block',
+  circle: ' btn-circle',
+  square: ' btn-square',
+}
+
+const COLORS = {
+  no: '',
+  primary: ' btn-primary',
+  secondary: ' btn-secondary',
+  accent: ' btn-accent',
+  info: ' btn-info',
+  success: ' btn-success',
+  warning: ' btn-warning',
+  error: ' btn-error',
+}
+
+const EFFECTS = {
+  no: '',
+  ghost: ' ghost',
+  link: ' link',
+  outline: ' outline',
+}
+
+const STATUS = {
+  no: '',
+  active: ' active',
+  disabled: ' disabled',
+}
+
+const GLASS = {
+  no: '',
+  glass: ' glass',
+}
+
+const LOADING = {
+  no: '',
+  loading: ' loading',
+}
+
+const ANIMATION = {
+  no: '',
+  noAnimation: ' no-animation',
+}
+
+type Props = {
+  text: string
+  size?: keyof typeof SIZES
+  shape?: keyof typeof SHAPES
+  color?: keyof typeof COLORS
+  effect?: keyof typeof EFFECTS
+  status?: keyof typeof STATUS
+  glass?: keyof typeof GLASS
+  loading?: keyof typeof LOADING
+  animation?: keyof typeof ANIMATION
+}
+
+const Button = ({
+  text,
+  size = 'no',
+  shape = 'no',
+  color = 'no',
+  effect = 'no',
+  status = 'no',
+  glass = 'no',
+  loading = 'no',
+  animation = 'no',
+}: Props): JSX.Element => {
+  return (
+    <button
+      className={clsx(
+        'btn',
+        SIZES[size],
+        SHAPES[shape],
+        COLORS[color],
+        EFFECTS[effect],
+        STATUS[status],
+        GLASS[glass],
+        LOADING[loading],
+        ANIMATION[animation]
+      )}
+    >
+      {text}
+    </button>
+  )
+}
 
 export default Button
