cb19c10808484a53f426bbc0d4cf5beeedd80ca3
sionnach <sionnach.solutions@gmail.com>
2021-08-03 10:36:23 -0400

    Adds NextWebVitalsMetric

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import type { AppProps } from 'next/app'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import { ReactElement, useEffect } from 'react'
 const ThemeChange = require('theme-change')
 const { themeChange } = ThemeChange
@@ -10,6 +10,19 @@ function MyApp({ Component, pageProps }: AppProps): ReactElement<any, any> {
     themeChange(false)
   }, [])
   return <Component {...pageProps} />
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric): void {
+  // const body =
+  JSON.stringify(metric)
+  // const url = 'https://example.com/analytics'
+
+  // // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  // if (navigator.sendBeacon) {
+  //  navigator.sendBeacon(url, body)
+  // } else {
+  //  fetch(url, { body, method: 'POST', keepalive: true })
+  // }
 }
 
 export default MyApp
