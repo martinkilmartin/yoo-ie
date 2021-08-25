aec5e365be3a8f6b7518b85c4e8c47cca8ed5273
sionnach <sionnach.solutions@gmail.com>
2021-08-25 13:29:57 -0400

    Adds favicons

--- components/HTMLHead/HTMLHead.tsx
+++ components/HTMLHead/HTMLHead.tsx
@@ -11,6 +11,11 @@ const HTMLHead = ({ title, description }: Props): JSX.Element => (
     <meta charSet="utf-8" />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta name="description" content={description} />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
   </Head>
 )
 
