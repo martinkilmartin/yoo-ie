73ead242f506a1f46888e27591009d36beee0b6b
sionnach <sionnach.solutions@gmail.com>
2021-08-27 12:50:04 -0400

    Directly add all theme links

--- components/Dropdown/Dropdown.tsx
+++ components/Dropdown/Dropdown.tsx
@@ -1,5 +1,3 @@
-import { ReactNode } from 'react'
-
 import clsx from 'clsx'
 
 const ALIGN = {
@@ -25,7 +23,6 @@ type Props = {
   align?: keyof typeof ALIGN
   openFrom?: keyof typeof OPEN_FROM
   open?: keyof typeof OPEN
-  children: ReactNode
 }
 
 const Dropdown = ({
@@ -33,7 +30,6 @@ const Dropdown = ({
   align = 'no',
   openFrom = 'bottom',
   open = 'no',
-  children,
 }: Props): JSX.Element => {
   return (
     <div
@@ -51,7 +47,111 @@ const Dropdown = ({
         tabIndex={0}
         className="p-2 shadow menu dropdown-content bg-base-100 text-base-content rounded-box w-52"
       >
-        {children}
+        <li>
+          <a tabIndex={0} data-set-theme="light" data-act-class="active">
+            🌝 light
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="dark" data-act-class="active">
+            🌚 dark
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="cupcake" data-act-class="active">
+            🧁 cupcake
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="bumblebee" data-act-class="active">
+            🐝 bumblebee
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="emerald" data-act-class="active">
+            ✳️ Emerald
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="corporate" data-act-class="active">
+            🏢 Corporate
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="synthwave" data-act-class="active">
+            🌃 synthwave
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="retro" data-act-class="active">
+            👴 retro
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="cyberpunk" data-act-class="active">
+            🤖 cyberpunk
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="valentine" data-act-class="active">
+            🌸 valentine
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="halloween" data-act-class="active">
+            🎃 halloween
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="garden" data-act-class="active">
+            🌷 garden
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="forest" data-act-class="active">
+            🌲 forest
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="aqua" data-act-class="active">
+            🐟 aqua
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="lofi" data-act-class="active">
+            👓 lofi
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="pastel" data-act-class="active">
+            🖍 pastel
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="fantasy" data-act-class="active">
+            🧚‍♀️ fantasy
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="wireframe" data-act-class="active">
+            📝 Wireframe
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="black" data-act-class="active">
+            🏴 black
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="luxury" data-act-class="active">
+            💎 luxury
+          </a>
+        </li>
+        <li>
+          <a tabIndex={0} data-set-theme="dracula" data-act-class="active">
+            🧛‍♂️ dracula
+          </a>
+        </li>
       </ul>
     </div>
   )
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import { CSSTheme } from '../../types'
 
 import { Dropdown } from '../Dropdown'
-import { ThemeChangeListLinks } from '../ListLinks'
 
 type Props = {
   logo?: JSX.Element
@@ -9,16 +8,14 @@ type Props = {
   themes: CSSTheme[]
 }
 
-const Navbar = ({ logo, brand, themes }: Props): JSX.Element => (
+const Navbar = ({ logo, brand }: Props): JSX.Element => (
   <div className="mb-2 shadow-lg navbar bg-primary text-primary-content rounded-box">
     {logo && <div className="flex-none">{logo}</div>}
     <div className="flex-1 px-2 mx-2">
       <span className="text-lg font-bold">{brand}</span>
     </div>
     <div className="flex-none">
-      <Dropdown align="end">
-        <ThemeChangeListLinks themes={themes} />
-      </Dropdown>
+      <Dropdown align="end" />
     </div>
   </div>
 )
