1a5eeabec642bc4c705c743a7663e60f1d9ce064
sionnach <sionnach.solutions@gmail.com>
2021-09-28 19:02:21 -0400

    Adds LogTail and logs NextWebVitals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { Browser as Logtail } from '@logtail/js'
 import { useEffect } from 'react'
+const LOGTAIL_SOURCE_TOKEN = process.env.NEXT_PUBLIC_LOGTAIL_SOURCE_TOKEN || ''
+
+const logtail = new Logtail(LOGTAIL_SOURCE_TOKEN)
 const ThemeChange = require('theme-change')
 const { themeChange } = ThemeChange
 
@@ -13,55 +17,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 }
 
 export function reportWebVitals(metric: NextWebVitalsMetric): void {
-  // handle Web Vitals
-  if (metric.label === 'web-vital') {
-    switch (metric.name) {
-      case 'FCP':
-        // handle FCP results
-        break
-      case 'LCP':
-        // handle LCP results
-        break
-      case 'CLS':
-        // handle CLS results
-        break
-      case 'FID':
-        // handle FID results
-        break
-      case 'TTFB':
-        // handle TTFB results
-        break
-      default:
-        break
-    }
-  }
-  // handle NextJS results
-  else if (metric.label === 'custom') {
-    switch (metric.name) {
-      case 'Next.js-hydration':
-        // handle hydration results
-        break
-      case 'Next.js-route-change-to-render':
-        // handle route-change to render results
-        break
-      case 'Next.js-render':
-        // handle render results
-        break
-      default:
-        break
-    }
-  }
-
-  // const body =
-  JSON.stringify(metric)
-  // const url = 'https://example.com/analytics'
-
-  // // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-  // if (navigator.sendBeacon) {
-  //  navigator.sendBeacon(url, body)
-  // } else {
-  //  fetch(url, { body, method: 'POST', keepalive: true })
-  // }
+  logtail.info(JSON.stringify(metric))
 }
 
 export default MyApp
