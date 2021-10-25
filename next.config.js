0b314c7bf9539afd33066ac3d039f1db5a559efe
sionnach <sionnach.solutions@gmail.com>
2021-10-25 16:48:32 -0400

    Adds next-eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
+  extends: ['plugin:@next/next/recommended'],
   reactStrictMode: true,
   pwa: {
     dest: 'public',
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,6 +1,8 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default (request: VercelRequest, response: VercelResponse): void => {
+const HelloWorld = (request: VercelRequest, response: VercelResponse): void => {
   const { name = 'World' } = request.query
   response.status(200).send(`Hello ${name}!`)
 }
+
+export default HelloWorld
