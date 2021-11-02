48ba94e8ccd0cb17f94336442b38e6e97266fed4
sionnach <sionnach.solutions@gmail.com>
2021-11-02 13:14:38 -0400

    Removes logtail and adds countries data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
-import type { AppProps, NextWebVitalsMetric } from 'next/app'
-import { Browser as Logtail } from '@logtail/js'
+import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
-const LOGTAIL_SOURCE_TOKEN = process.env.NEXT_PUBLIC_LOGTAIL_SOURCE_TOKEN || ''
 
-const logtail = new Logtail(LOGTAIL_SOURCE_TOKEN)
 const ThemeChange = require('theme-change')
 const { themeChange } = ThemeChange
 
@@ -14,10 +11,6 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     themeChange(false)
   }, [])
   return <Component {...pageProps} />
-}
-
-export function reportWebVitals(metric: NextWebVitalsMetric): void {
-  logtail.info(JSON.stringify(metric))
 }
 
 export default MyApp
