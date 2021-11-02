63bf67fe0ab8202fcf85b0f692dfda7e699a1459
sionnach <sionnach.solutions@gmail.com>
2021-11-02 15:51:34 -0400

    MiddleWare modifies request

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -9,6 +9,7 @@ export async function middleware(req: NextRequest): Promise<Response> {
   const country = geo.country || 'US'
 
   if (ALLOWED_COUNTRY.includes(country)) {
+    url.searchParams.set('country', country)
     return NextResponse.rewrite(url)
   } else if (BLOCKED_COUNTRY.includes(country)) {
     return new Response('Blocked for testing reasons', { status: 451 })
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,8 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
 const HelloWorld = (request: VercelRequest, response: VercelResponse): void => {
-  const { name = 'World' } = request.query
-  response.status(200).send(`Hello ${name}!`)
+  response.status(200).send(`Hello ${request.query.country}!`)
 }
 
 export default HelloWorld
