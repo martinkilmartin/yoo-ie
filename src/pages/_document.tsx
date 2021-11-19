75e4bc7601ef7eea8c9603333793113a0e3f33a6
Martin Kilmartin <76404720+martinkilmartin@users.noreply.github.com>
2021-11-19 00:43:20 -0500

    Sets default theme to forest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,7 +18,7 @@ class MyDocument extends Document {
 
   render(): JSX.Element {
     return (
-      <Html data-theme="light" lang="en">
+      <Html data-theme="forest" lang="en">
         <Head />
         <body className="bg-base-100">
           <Main />
