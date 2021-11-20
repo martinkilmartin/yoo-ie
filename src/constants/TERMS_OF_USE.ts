e900306225fae8900d26bab1843edc639e03067b
Martin Kilmartin <76404720+martinkilmartin@users.noreply.github.com>
2021-11-19 22:02:43 -0500

    Adds TERMS_OF_USE data

---
+++ src/constants/TERMS_OF_USE.ts
@@ -0,0 +1,95 @@
+const TermsOfUse = [
+  {
+    title: '🪧 1. About these Terms of Use',
+    content: [
+      'Welcome to Sionnach Solutions!',
+      'Last updated: 19 November 2021',
+      'We are Sionnach Buí Limited incorporated in Ireland under company registration number 640733. Our registered office is at H91 VH4C.',
+      'We are also Sionnach Solutions incorporated in Canada as 9196692 Canada Inc. Our registered office is at 1870 Rue Mullins, Montréal, QC H3K 1N8, Canada.',
+      'These Terms of Use apply to the use of this website, regardless of how you access it. Please read these Terms of Use carefully before you proceed.',
+      'We may, at any time and without notice, terminate your access to or use of this website. If we do so, you do not have the right to bring any claim or claims against us.',
+    ],
+  },
+  {
+    title: '✅ 2. Consent to Terms of Use',
+    content: [
+      'By using this website you agree to these Terms of Use.',
+      'If you do not agree to these Terms of Use, please do not ask us to find you a professional or use this website. These Terms of Use were last updated on the date shown at the top. We may change these Terms of Use at any time by posting an updated version on our website, so you may wish to check it before using this website. You may only use this website for lawful purposes.',
+    ],
+  },
+  {
+    title: '✔ 3. Our liability',
+    content: [
+      'Nothing in these terms limits our liability for death or personal injury caused by our negligence, fraud or fraudulent misrepresentation or for any matter that we cannot exclude or limit as a matter of law.',
+      'We provide the website to Customers free of charge and on an “as is” basis. We introduce Customers to Professionals to provide the services described on the website, and are not responsible or liable for the end services that are provided.',
+      'Therefore, to the extent permitted by law, (a) we disclaim any warranties, express or implied, and (b) other than as set out above, we are not liable for any loss or damage — direct or indirect and whether arising in contract, tort, or otherwise — even if we have been advised of the possibility of this loss or damage.',
+      'These limitations don’t affect your statutory rights under the Consumer Protection Act 2007, if applicable and any laws that replace it. If you want independent advice about your rights, you can speak to Citizens Information or the European Consumer Centre.',
+    ],
+  },
+  {
+    title: '📨 4. If we need to talk to you',
+    content: [
+      'From time to time, we might need to contact you about your user account. We’ll normally do that over email, which is why we need a valid email address from you if you agree to make a user account through the website.',
+    ],
+  },
+  {
+    title: '📧 5. If you need to talk to us',
+    content: [
+      'You can contact us through our Contact Us page on the website.',
+    ],
+  },
+  {
+    title: '💾 6. Monitoring phone calls and emails',
+    content: [
+      'We may monitor and record our calls and emails, so we can find out where we need to improve and check we’re keeping to our normal practises and procedures.',
+    ],
+  },
+  {
+    title: '© 7. Copyright notice',
+    content: [
+      'Unless we expressly state otherwise, the copyright and any other intellectual property rights, including but not limited to design rights, trade marks and patents appearing anywhere on this website remain our property, whether owned by or licensed to us.',
+      'You may not use any of the material on this website without our prior written permission for your own commercial purposes, whether by reproducing, copying, downloading, printing, linking to, editing, broadcasting, distributing or otherwise. You may use it for your own personal non-commercial use.',
+    ],
+  },
+  {
+    title: '⚠ 8. Disclaimer',
+    content: [
+      'Accessing or using this website or its content in any way is done entirely at your own risk. You will be responsible for any loss or damage to any computer, device, software, systems or data resulting directly or indirectly from the use or inability to use this website or its content.',
+      'We are under no obligation to provide uninterrupted access to this website. We reserve the right to restrict your access to this website at any time and for any reason.',
+      'We do not guarantee that the contents of this website will be free of errors, bugs, worms, trojans or viruses or otherwise make any representations as to the quality or accuracy or completeness of the content available on the website including, but not limited to any price quotes, stock availability data or non-fraudulent representations. You are responsible for maintaining appropriate software on your computer or device to protect you from any such errors, bugs, worms, trojans or viruses.',
+      'To the fullest extent permissible by law, we exclude any and all liability to you resulting from your use of the website or connected to these Terms of Use. This exclusion includes but is not limited to any type of damages, loss of data, income or profit or loss or damage to property belonging to you or third parties arising from the use of this website or its contents.',
+      'Nothing in these Terms of Use is intended to limit our liability to you for death or personal injury resulting from our negligence or that of our employees or agents.',
+    ],
+  },
+  {
+    title: '↗ 9. Links to third party websites',
+    content: [
+      'This website may provide links out to websites or other online resources under the control of third parties. Any such links are provided solely for your convenience. We have no control over the contents of these third-party resources. We are not responsible for the contents of any linked websites and do not endorse them in any way.',
+    ],
+  },
+  {
+    title: '↙ 10. Links from third party websites',
+    content: [
+      'You can only link to our website with prior written permission from us. We reserve the right to withdraw any such permission at any time.',
+    ],
+  },
+  {
+    title: '㊙ 11. Privacy policy',
+      'We take your privacy and the protection of your data very seriously. We may gather and/or use certain information about you in accordance with our privacy policy. Please see our separate privacy policy for more information.',
+    ],
+  },
+  {
+    title: '🤝 12. Entire agreement',
+    content: [
+      'These Terms of Use are the entire agreement between us and you, and supersede any and all prior terms, conditions, warranties or representations to the fullest extent permitted by law.',
+    ],
+  },
+  {
+    title: '🇮🇪 13. Applicable law',
+    content: [
+      'This Agreement shall be governed by the law of Ireland and courts of Ireland will have exclusive jurisdiction in relation to these Terms of Use.',
+    ],
+  },
+]
+
+export default TermsOfUse
