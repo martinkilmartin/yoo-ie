b9a6535b31cf75bf4fbee9e6e2929627751cdb50
sionnach <sionnach.solutions@gmail.com>
2021-11-27 13:24:25 -0500

    Use dark theme for dark mode...

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -7,7 +7,7 @@ type Props = {
   b?: string
 }
 
-const ThemeToggle = ({ a = 'cmyk', b = 'forest' }: Props): JSX.Element => {
+const ThemeToggle = ({ a = 'cmyk', b = 'dark' }: Props): JSX.Element => {
   const [theme, setTheme] = useState(true)
   const toggleTheme = () => setTheme((theme) => !theme)
   return (
